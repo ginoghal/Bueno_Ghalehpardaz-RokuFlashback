6ae4757de91c0a583a3017e3e208a99e0dfcff57
Gino Ghalehpardaz <ginoghalehpardaz@Ginos-MacBook-Pro-2.local>
2020-04-11 21:24:06 -0400

    css

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -26,7 +26,7 @@ if(isset($_POST['submit'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/reset.css">
+    <link rel="stylesheet" href="../public/css/reset.css">
     <link rel="stylesheet" href="../public/css/style.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Roku Flashback App</title>
